99817e0ffbe96a53e47d362a2f2182a5fba56f28
habibmuhammadthariq <thariqhabibmuhammad@gmail.com>
2022-01-26 19:08:14 +0700

    yes

--- simple_find_contours_v2.py
+++ simple_find_contours_v2.py
@@ -9,29 +9,29 @@ print("Frame Per Second : {}".format(fps))
 
 def find_contours(image):
     # convert image into gray and blur it
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (3, 3), cv2.BORDER_DEFAULT)
-    erosion = cv2.erode(blur, (5, 5))
+    blur = cv2.GaussianBlur(image, (3, 3), cv2.BORDER_DEFAULT)
+    gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+
     # detect edge
-    canny = cv2.Canny(erosion, 100, 200)
+    canny = cv2.Canny(gray, 100, 200)
+
+    kernel = np.ones((3,3), np.uint8)
+    # dilation = cv2.
+    erosion = cv2.dilate(canny, kernel, iterations=3)
 
     # find contours
     cnts, hier = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    # get the biggest 5
+    # get the biggest 8
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:8]
 
     contours = []
-    # x = []
-    # y = []
-    # w = []
-    # h = []
     cx = []
     cy = []
     # loop over the contours
     for c in cnts:
         peri = cv2.arcLength(c, True)  # perimeter/ keliling. True -> the contour was closed/ connected
-        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        approx = cv2.approxPolyDP(c, 0.03 * peri, True)
 
         # get the quadrilateral contour. quadrilateral -> berbentuk persegi empat
         if len(approx) == 4:
@@ -42,23 +42,11 @@ def find_contours(image):
             if M["m00"] == 0: M["m00", "m01"] = 1
             cx.append(int(M["m10"] / M["m00"]))
             cy.append(int(M["m01"] / M["m00"]))
-            # box = cv2.boundingRect(c)
-            # x.append(box[0])
-            # y.append(box[1])
-            # w.append(box[2])
-            # h.append(box[3])
-            #  get center x and y of the contour
-            # cx.append(box[0] + (box[2] / 2))  # x + (w/2)
-            # cy.append(box[1] + (box[3] / 2))  # y + (h/2)
-            # we can stop the loop using "break" command or
-            # just loop over every single contours in case of there are more than one quadrilateral contour
 
     return contours, hier, cx, cy, canny
 
 
 def get_contours_detail(cnts):
-    # x = []
-    # y = []
     w = []
     h = []
     cx = []
@@ -66,10 +54,9 @@ def get_contours_detail(cnts):
 
     for c in cnts:
         box = cv2.boundingRect(c)
-        # x.append(box[0])
-        # y.append(box[1])
         w.append(box[2])
         h.append(box[3])
+
         # get center x and y of the contour
         cx.append(box[0] + (box[2] / 2))  # x + (w/2)
         cy.append(box[1] + (box[3] / 2))  # y + (h/2)
@@ -81,21 +68,25 @@ def get_contours_with_same_area(cnts):
     contours = []
     areas = []
 
-    length = len(cnts) - 2
-    i = 1
-    for c in cnts:
-        if not contours:
-            contours.append(c)
-            areas.append(cv2.contourArea(c))
-            continue
+    for i in range(len(cnts)-2):
+        contours.append(cnts[i])
+        areas.append(cv2.contourArea(cnts[i]))
 
-        if i <= length:
-            area = cv2.contourArea(c)
-            if areas[0] - 20 <= area <= areas[0] + 20:
-                contours.append(c)
+        for j in range(len(cnts)):
+            if j == i:
+                continue
 
+            area = cv2.contourArea(cnts[j])
+            if areas[0]-80 <= area <= areas[0]+80:
+                contours.append(cnts[j])
+        
+        if len(contours) == 2 or len(contours) == 3:
+            break
+        else:
+            contours.clear()
+            areas.clear()
+    
     return contours
-
 
 def get_box(cnts, w, h, cx, cy):
     xL = 0
@@ -106,8 +97,7 @@ def get_box(cnts, w, h, cx, cy):
     half_height = h[0] / 2
 
     if len(cnts) == 3:
-
-        if cx[1] - 10 <= cx[0] <= cx[1] + 10:  # toleransi 10 pixel
+        if cx[1] - 20 <= cx[0] <= cx[1] + 20:  # toleransi 20 pixel
             # get y left and y right
             if cy[0] < cy[1]:
                 yL = cy[0]
@@ -123,7 +113,7 @@ def get_box(cnts, w, h, cx, cy):
             else:
                 xL = cx[2]
                 xR = cx[0]
-        elif cy[1] - 10 <= cy[0] <= cy[1] + 10:
+        elif cy[1] - 20 <= cy[0] <= cy[1] + 20:
             # get x left and x right
             if cx[0] < cx[1]:
                 xL = cx[0]
@@ -140,13 +130,33 @@ def get_box(cnts, w, h, cx, cy):
                 yL = cy[0]
                 yR = cy[2]
 
+    # temporary
+    elif len(cnts) == 2:
+        if cx[0] < cx[1]:
+            xL = cx[0]
+            xR = cx[1]
+        else:
+            xL = cx[1]
+            xR = cx[0]
+        
+        if cy[0] < cy[1]:
+            yL = cy[0]
+            yR = cy[1]
+        else:
+            yL = cy[1]
+            yR = cy[0]
+    
+    else:
+        pass
+
+
     xL = xL - half_width
     yL = yL - half_height
     xR = xR + half_width
     yR = yR + half_height
     cx = xL + (xR - xL) / 2
     cy = yL + (yR - yL) / 2
-    # return xL, yL, xR, yR
+    
     return xL, yL, xR, yR, cx, cy
 
 
@@ -169,7 +179,6 @@ while True:
     width_img, height_img, channel_img = frame.shape
     cx_img = height_img / 2
     cy_img = width_img / 2
-    # print("width : {}, height : {}".format(width_img,height_img))
 
     if not ret:
         break
@@ -179,33 +188,35 @@ while True:
 
     # draw contours found
     drawing = np.zeros((canny.shape[0], canny.shape[1], 3), dtype=np.uint8)
+    cv2.drawContours(drawing, contours, -1, (0, 255, 0), 2) # , cv2.LINE_8, hierarchy, 0)
     for i in range(len(contours)):
-        cv2.drawContours(drawing, contours, i, (0, 255, 0), 2, cv2.LINE_8, hierarchy, 0)
-        cv2.putText(drawing, "{},{}".format(cx[i], cy[i]), (int(cx[i]), int(cy[i])), cv2.FONT_HERSHEY_SIMPLEX, 0.4,
+        cv2.putText(drawing, "{} : {},{}".format(i, cx[i], cy[i]), (int(cx[i]), int(cy[i])), cv2.FONT_HERSHEY_SIMPLEX, 0.4,
                     (0, 0, 255), 1, cv2.LINE_AA)
+
     # show it up
     cv2.imshow("Result", drawing)
 
-    # get contours with the same size area
-    for i in range(1, len(contours) - 2):
-        contours = get_contours_with_same_area(contours)
-        if 3 >= len(contours) > 1:  # take only 2 or 3 contours
-            break
-    # get the detail contours
-    w, h, cx, cy = get_contours_detail(contours)
+    # get same contours area
+    cnts = get_contours_with_same_area(contours)
+    print("Contours length : {}".format(len(cnts)))
 
-    if 3 >= len(contours) > 1:
-        x1, y1, x2, y2, cx, cy = get_box(contours, w, h, cx, cy)
-        # print("x1 : {}, y1 : {}, x2 : {}, y2 : {}".format(x1, y1, x2, y2))
-        # print("{}, {}, {}, {}".format(type(x1),type(y1),type(x2),type(y2)))
+    if len(cnts) == 3:
+        # get the detail contours
+        w, h, cx, cy = get_contours_detail(cnts)
+
+        # rectangle
+        x1, y1, x2, y2, cx, cy = get_box(cnts, w, h, cx, cy)
+        
         cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
         cv2.putText(frame, "{},{}".format(cx, cy), (int(cx), int(cy)), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                     (0, 0, 255), 2, cv2.LINE_AA)
 
+        cv2.circle(frame, (int(cx), int(cy)), 5, (0,255,0), cv2.FILLED)
+        cv2.circle(frame, (int(cx_img), int(cy_img)), 5, (0,255,0), cv2.FILLED)
+
         # distance between center object to center image
         cv2.line(frame, (int(cx), int(cy)), (int(cx_img), int(cy_img)), (0, 255, 0), 2)
         cx_obj_img, cy_obj_img, direction = get_direction(cx, cy, cx_img, cy_img)
-        # print(int(cx_obj_img) + int(cy_obj_img))
         cv2.putText(frame, direction, (int(cx_obj_img), int(cy_obj_img)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0),
                     1, cv2.LINE_AA)
 
